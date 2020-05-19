f38368ada98a5b0ce21c3825a2be4decd205ec76
Takahisa Sakuma <tsheadbang@gmail.com>
2020-05-19 19:26:05 +0900

    add firebase, base css

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,44 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-firebase-auth
-      </h1>
-      <h2 class="subtitle">
-        My tremendous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="centered">
+      <div>
+        <h1 class="title">nuxt-firebase-auth</h1>
+        <form>
+          <div class="form-input">
+            <input v-model="email" type="email" placeholder="email" />
+          </div>
+          <div class="form-input">
+            <input type="password" placeholder="password" />
+          </div>
+          <div class="buttons">
+            <button class="button">sign up</button>
+            <button class="button">log in</button>
+          </div>
+        </form>
+
+        <div class="links">
+          <nuxt-link to="secret" class="button--green">secret page</nuxt-link>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import firebase from '~/plugins/firebase'
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
-  margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
@@ -48,25 +46,22 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.centered {
+  background-color: wheat;
+  width: 100%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+form {
+  max-width: 450px;
+  margin: 20px auto;
+  // max-width: 450px;
+  background-color: aquamarine;
 }
-
-.links {
-  padding-top: 15px;
+.form-input {
+  margin: 15px auto;
+  input {
+    width: 90%;
+    max-width: 300px;
+  }
 }
 </style>
